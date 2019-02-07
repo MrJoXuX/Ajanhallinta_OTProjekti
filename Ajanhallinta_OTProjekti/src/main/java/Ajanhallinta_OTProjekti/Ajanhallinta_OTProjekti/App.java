ed4cefa81e3c184cca7aeebfce9f7516ce79ac26
Joackim Degerlund <joackimd@metropolia.fi>
2019-02-07 16:43:38 +0200

    Created Maven project
    Created the base for the Maven Project

---
+++ Ajanhallinta_OTProjekti/src/main/java/Ajanhallinta_OTProjekti/Ajanhallinta_OTProjekti/App.java
@@ -0,0 +1,13 @@
+package Ajanhallinta_OTProjekti.Ajanhallinta_OTProjekti;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
